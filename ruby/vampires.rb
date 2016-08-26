5c5ccaa327cb08ae4466b86f6500192773715194
d.seleshi.h <davucan@gmail.com>
2016-08-26 14:35:14 -0700

    Release 0 questioneer

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name: "
+name=gets.chomp
+puts "How old are you?"
+age=gets.chomp.to_i
+puts "What year were you born?"
+year=gets.chomp.to_i
+puts "our company cafeteria serves bread. Should we order some for you?"
+garlic=gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+health-insurance=gets.chomp
