2378b45b511c76724413c0b39c6e110a8e7d9ed2
d.seleshi.h <davucan@gmail.com>
2016-09-02 15:25:05 -0700

    Add swap method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,3 +4,18 @@ def next_vowel (ch)
    ch == 'u' ?
    'a': vowels[vowels.index(ch)+1]
 end
+next_vowel 'a'
+def next_consonant (ch)
+   ch.downcase!
+   consonants='bcdfghjklmnpqrstvwxyz'
+   ch == 'z' ?
+   'b': consonants[consonants.index(ch)+1]
+end
+next_consonant 'z'
+def swap (string)
+    string=string.split " "
+	temp=string[0]
+	string[0]=string[1]
+	string[1]=temp
+	string.join " "
+end
