4cff98a55df3047c1dbebd8d199daaa7d763014f
d.seleshi.h <davucan@gmail.com>
2016-09-03 12:13:19 -0700

    Add fibonaci series and intergrate with search array method

--- ruby/problem_solving_method.rb
+++ ruby/problem_solving_method.rb
@@ -8,11 +8,29 @@ def search_array(arr,integer)
   end
   if !instances.empty?
   	instances.each{|x| puts "#{integer} is at #{x} position in the array"}
+    instances
   else
   	puts "Integer not in the array!"
   end
 end
-array=[1,2,2,1]
-# if search_array(array) == array.length
 
-search_array(array,10)
+
+# array=[1,2,2,1]
+def fib(num)
+  fib_array = [0,1]
+  if num > 2 
+    for i in 2...num do
+      fib_array[i]=fib_array[i-1]+fib_array[i-2]
+    end
+  elsif num == 2 
+    fib_array
+  elsif num == 1 
+      fib_array.delete(1) 
+  else
+    puts "Number to small"
+  end
+  fib_array
+end
+
+fib 6
+search_array fib(100), 218922995834555169026
