f62c609b6375c9b9759be959e019740f57a9e48f
d.seleshi.h <davucan@gmail.com>
2016-09-07 12:51:46 -0700

    Add santa methods

---
+++ ruby/santa.rb
@@ -0,0 +1,11 @@
+class Santa
+	def speak
+		p "Ho, ho, ho! Haaaappy holidays!"
+	end
+	def eat_milk_and_cookies(cookie_type)
+		p "That was a good #{cookie_type}!"
+	end
+	def initialize
+		p "Initializing Santa instance ..."
+	end
+end
