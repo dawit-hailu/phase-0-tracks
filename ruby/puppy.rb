a03848dab6ea350257d609854d894edad2a33b37
d.seleshi.h <davucan@gmail.com>
2016-09-07 12:26:24 -0700

    Add puppy characterstics

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# Breed ------------------------
+# Canine
+
+# CHARACTERISTICS ----------------
+# Name: varies
+# Age: varies
+# Gender: varies
+# Color: varies
+# Bark vol: 8
+# Shadding level: 2
+
+
+# BEHAVIOR -----------------------
+# Aggressive: varies
+# Playfull: yes
