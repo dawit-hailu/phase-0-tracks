38c1dbaa3b1f0e2ab3986c4028b823b02d3510c7
d.seleshi.h <davucan@gmail.com>
2016-09-09 00:14:46 -0700

    Add mixin module Shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,32 @@
-module Shout
-	def self.yell_angrily(words)
-      words + "!!!" + " :("
-    end
+# module Shout
+# 	def self.yell_angrily(words)
+#       words + "!!!" + " :("
+#     end
     
-    def self.yell_happily(words)
-      words + "!!!" + " :D"
+#     def self.yell_happily(words)
+#       words + "!!!" + " :D"
+#     end
+# end
+
+# p Shout.yell_happily("Ureka!!")
+# p Shout.yell_angrily("Darn it!")
+
+module Shout
+	def yell(words)
+      words + "!!!"
     end
 end
+class Programmer
+	include Shout
+end
+class Person
+	include Shout
+end
 
-p Shout.yell_happily("Ureka!!")
-p Shout.yell_angrily("Darn it!")
+rubyist = Programmer.new()
+
+puts rubyist.yell("I love Ruby")
+
+donald = Person.new()
+
+puts donald.yell("Get him outta here")
