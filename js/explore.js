95e5dc2bc1f748061c9bf5ef2319f2ee7552f222
d.seleshi.h <davucan@gmail.com>
2016-09-13 13:56:06 -0700

    Add function to reverse string

--- js/explore.js
+++ js/explore.js
@@ -6,3 +6,12 @@
   -iterate through all characters
   -append each character to a new variable
   -return new variable*/
+function reversString(str) {
+	var newString = "";
+	var stringLength = str.length;
+	for (i=stringLength; i > -1; i--) {
+	newString += str.charAt(i);
+	}
+	return newString;
+}  
+console.log(reversString("Hello"));
