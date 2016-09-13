94227ab138a8ec0febebedaca230aa85fe217185
d.seleshi.h <davucan@gmail.com>
2016-09-13 13:55:14 -0700

    Add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+//Pseudocode
+
+/*-create a function that takes an input string
+   inside the function
+  -find out number of charcters
+  -iterate through all characters
+  -append each character to a new variable
+  -return new variable*/
