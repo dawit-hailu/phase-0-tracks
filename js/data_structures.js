a557f139a8bf6c157ff03d1d7fbded9ecfdb9cfe
d.seleshi.h <davucan@gmail.com>
2016-09-15 13:47:06 -0700

    Add js car object

--- js/data_structures.js
+++ js/data_structures.js
@@ -9,5 +9,13 @@ function car(color,year,brand,model) {
 	this.brand = brand;
 	this.model = model;
 }
-mycar = new car('orange',1931, 'ford', 'T' );
-console.log(mycar.color +" "+ mycar.year +" "+ mycar.brand)
+var cars = [];
+cars.push(new car('orange',1931, 'ford', 'T' ));
+cars.push(new car('red',2001, 'ford', 'escape' ));
+cars.push(new car('green',2015, 'toyota', 'corola' ));
+cars.push(new car('silver',2014, 'tesla', 'Model S' ));
+
+
+for (var i=0; i < cars.length; i++) {
+	console.log(cars[i].color +" "+ cars[i].year +" "+ cars[i].brand +" "+ cars[i].model)
+}
